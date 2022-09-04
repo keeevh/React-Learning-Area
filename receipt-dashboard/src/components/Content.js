7f8b106e516de0f45259010acc492eea3a594c7b
Kevin Henrich <kevinhenrich@icloud.com>
2022-09-04 22:09:11 +0200

    added search bar, settings icon and go premium cta

--- receipt-dashboard/src/components/Content.js
+++ receipt-dashboard/src/components/Content.js
@@ -1,8 +1,20 @@
 import "../css/Content.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faSliders } from "@fortawesome/free-solid-svg-icons";
 
 export default function Content() {
   return (
     <div>
+      <div className="content-top-bar">
+        <div className="search-bar">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <input type="text" placeholder="Enter your search request..."></input>
+        </div>
+        <div className="content-top-bar-right">
+          <FontAwesomeIcon icon={faSliders} />
+          <button>Go to Premium</button>
+        </div>
+      </div>
     </div>
   );
 }
