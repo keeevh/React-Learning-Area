3c0e174fa95f05351d70a7c5c17542c6b2f3ad37
Kevin Henrich <kevinhenrich@icloud.com>
2022-09-04 21:28:07 +0200

    imported Menu.js and Content.js modules

--- receipt-dashboard/src/App.js
+++ receipt-dashboard/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import MenuBar from "./components/Menu.js";
+import Content from "./components/Content.js";
+import "./css/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="menu-bar">
+        <MenuBar />
+      </div>
+      <div className="content-container">
+        <Content />
+      </div>
     </div>
   );
 }
