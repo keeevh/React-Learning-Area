f8caf6721e0e8aba9a6a1dced92efc1dc4d0a76a
Kevin Henrich <kevinhenrich@icloud.com>
2022-09-07 08:02:04 +0200

    added map component to render tasks from object array

--- simple-to-do-list/src/components/TaskContent.js
+++ simple-to-do-list/src/components/TaskContent.js
@@ -1,2 +1,47 @@
 import React from "react";
 import "../stylesheets/taskcontent.css";
+
+let tasks = [
+  {
+    title: "Clean Apartment",
+    category: "❤️ Personal",
+    status: "open",
+  },
+  {
+    title: "Gym Workout",
+    category: "💪🏽 Fitness",
+    status: "open",
+  },
+  {
+    title: "Finish Presentation",
+    category: "⌚ Business",
+    status: "open",
+  },
+  {
+    title: "Improve React Skills",
+    category: "🎓 Learning",
+    status: "open",
+  },
+];
+
+function RenderTasks() {
+    const allTasks = tasks.map((task) => (
+      <div className="task-card">
+        <input type="text" value={task.title} disabled></input>
+        <div className="task-card-lower">
+          <p>{task.category}</p>
+          <div>
+            <button>Edit</button>
+            <button>Delete</button>
+            <button>Check</button>
+          </div>
+        </div>
+      </div>
+    ));
+    const openTasks = allTasks.filter(task => {task.status = "open"});
+    return(
+        <div>{openTasks}</div>
+    )
+}
+
+export default RenderTasks;
